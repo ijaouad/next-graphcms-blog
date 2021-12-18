95490a22acbac057854a574478e5fba979e54af2
JAOUAD ICHI <ichijawad@gmail.com>
2021-12-18 14:27:50 +0100

    initial commit

---
+++ pages/post/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostDetails = () => {
+    return (
+        <div className="container mx-auto">
+            Post Details
+        </div>
+    )
+}
+
+export default PostDetails
